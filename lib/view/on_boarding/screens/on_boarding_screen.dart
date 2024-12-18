96f80803be94109fa7daaf81f1f275a88d628f10
salah <salahswidan212@gmail.com>
2024-12-18 19:21:17 +0200

    add package svgPicture

--- lib/view/on_boarding/screens/on_boarding_screen.dart
+++ lib/view/on_boarding/screens/on_boarding_screen.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class OnBoardingScreen extends StatelessWidget {
   const OnBoardingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    PageController pageController = PageController();
+    return Scaffold(
+      body: Column(
+        children: [
+          SvgPicture.asset(
+            "assets/images/onboardingImage(1).svg",
+          )
+        ],
+      ),
+    );
   }
 }
