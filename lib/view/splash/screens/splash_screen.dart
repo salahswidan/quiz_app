500f8bc8dfc98f23be3315fc86e93adf53440433
salah <salahswidan212@gmail.com>
2024-12-18 14:04:16 +0200

    finsh splash screen

--- lib/view/splash/screens/splash_screen.dart
+++ lib/view/splash/screens/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:quiz_app_new/core/resources/color_manager.dart';
 
 class SplashScreen extends StatelessWidget {
@@ -9,9 +10,34 @@ class SplashScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: ColorManager.kPrimaryColor,
       body: Center(
-        child: Text(
-          "Q",
-          style: TextStyle(fontSize: 400, color: Colors.white),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+              "Q",
+              style: GoogleFonts.montserrat(
+                fontSize: 400,
+                color: Colors.white,
+              ),
+            ),
+            MaterialButton(
+              onPressed: () {},
+              child: Container(
+                width: double.infinity,
+                height: 70,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(35)),
+                child: Center(
+                  child: Text(
+                    "Get Started",
+                    style: GoogleFonts.montserrat(
+                        fontSize: 21, color: Colors.black),
+                  ),
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
