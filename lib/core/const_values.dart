6b2f49c1fd0518028d862e477e8476c4800dc351
salah <salahswidan212@gmail.com>
2024-12-19 18:14:31 +0200

    add values of onboarding page

--- lib/core/const_values.dart
+++ lib/core/const_values.dart
@@ -1,9 +1,14 @@
 import '../models/on_boarding_screen/on_boarding_screen_model.dart';
 
 class ConstValues {
-static  List<OnBoardingScreenModel> onBoardingListConst = [
-   OnBoardingScreenModel( "assets/images/onboardingImage(1).svg", "Synonyms for quiz", "onBoardingDesc"),
-   OnBoardingScreenModel( "assets/images/onboardingImage(2).svg", "Synonyms for quiz", "onBoardingDesc"),
-   OnBoardingScreenModel( "assets/images/onboardingImage(3).svg", "Synonyms for quiz", "onBoardingDesc"),
+  static List<OnBoardingScreenModel> onBoardingListConst = [
+    OnBoardingScreenModel("assets/images/onboardingImage(1).svg",
+        "Synonyms for quiz", "Synonyms for quiz"),
+    OnBoardingScreenModel(
+        "assets/images/onboardingImage(2).svg",
+        "Antonyms for quiz",
+        "Sunt in cuipa qui officia deserunt mollit anim id est laborum."),
+    OnBoardingScreenModel("assets/images/onboardingImage(3).svg", "Cool quiz",
+        "Culpa qui officia deserunt mollit anim id est laborum."),
   ];
 }
--- lib/view/on_boarding/widgets/custom_items_page_view_on_boarding.dart
+++ lib/view/on_boarding/widgets/custom_items_page_view_on_boarding.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
@@ -6,42 +5,41 @@ import '../../../models/on_boarding_screen/on_boarding_screen_model.dart';
 
 class CustomItemsPageViewOnBoarding extends StatelessWidget {
   const CustomItemsPageViewOnBoarding({
-    super.key, required this.onBoardingScreenModel,
+    super.key,
+    required this.onBoardingScreenModel,
   });
-    final OnBoardingScreenModel onBoardingScreenModel;
-
+  final OnBoardingScreenModel onBoardingScreenModel;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 46.0),
-        child: Center(
-          child: SingleChildScrollView(
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SvgPicture.asset(
-                    onBoardingScreenModel.onBoardingImage,
-                    height: 400,
-                  ),
-                  SizedBox(
-                    height: 108,
-                  ),
-                  Text(
-                    onBoardingScreenModel.onBoardingTitle,
-                    style: TextStyle(
-                        fontSize: 32, fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(
-                    height: 24,
-                  ),
-                  Text(
-                    onBoardingScreenModel.onBoardingDesc,
-                    style: TextStyle(fontSize: 21),
-                  ),
-                ]),
-          ),
+      padding: const EdgeInsets.symmetric(horizontal: 46.0),
+      child: Center(
+        child: SingleChildScrollView(
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+            SvgPicture.asset(
+              onBoardingScreenModel.onBoardingImage,
+              height: 400,
+            ),
+            SizedBox(
+              height: 108,
+            ),
+            Text(
+              onBoardingScreenModel.onBoardingTitle,
+              style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 24,
+            ),
+            Text(
+              textAlign: TextAlign.center,
+              onBoardingScreenModel.onBoardingDesc,
+              style: TextStyle(fontSize: 21),
+            ),
+          ]),
         ),
-      );
+      ),
+    );
   }
 }
